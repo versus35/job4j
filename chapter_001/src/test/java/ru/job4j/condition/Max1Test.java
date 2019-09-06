de2fa234e4222f42cbda2691559d6d1a6d2a7fb7
BulankinViktor <bulankin.vs@gmail.com>
2019-09-06 23:51:53 +0300

    Max1 [159464]

---
+++ chapter_001/src/test/java/ru/job4j/condition/Max1Test.java
@@ -0,0 +1,27 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+
+    public class Max1Test {
+        @Test
+        public void whenMax1To2Then2() {
+            int result = Max.max(1, 2);
+            assertThat(result, is(2));
+        }
+        @Test
+        public void whenMax2To1Then1(){
+            int result = Max.max(4, 3);
+            assertThat(result, is (4));
+        }
+        @Test
+        public  void whenEqual2To1(){
+            int result = Max.max(5, 5);
+            assertThat(result, is (5));
+        }
+    }
+
+
