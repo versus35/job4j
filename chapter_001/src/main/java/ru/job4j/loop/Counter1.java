0a13843abfc34914f57c4800283d919ce98837a1
BulankinViktor <bulankin.vs@gmail.com>
2019-09-07 13:23:48 +0300

    Сounter1 [159465]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Counter1.java
@@ -0,0 +1,14 @@
+package ru.job4j.loop;
+
+public class Counter1 {
+    public int add(int start, int finish){
+        int sum = 0;
+        for(int i = start; i <=finish ; i ++){
+            if(i % 2 ==0){
+                sum +=i;
+            }
+
+        }
+        return sum;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/loop/Counter1Test.java
@@ -0,0 +1,24 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class Counter1Test {
+    @Test
+    public void whenSumEvenNumbersFromOneToTenThenThirty() {
+        Counter count = new Counter();
+        int result = count.add(1, 10);
+        assertThat(result, is(30));
+        //напишите здесь тест, проверяющий, что сумма чётных чисел от 1 до 10 при вызове метода counter.add будет равна 30.
+    }
+
+    @Test
+    public void whenSumEvenNumbersFrom1ToTenThenThirty() {
+        Counter count = new Counter();
+        int result = count.add(10, 15);
+        assertThat(result, is(36));
+    }
+}
+
