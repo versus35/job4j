7f08426923fed08f78158ca3b206840a0450711c
BulankinViktor <bulankin.vs@gmail.com>
2019-09-07 18:19:36 +0300

    FitnessCalc [159468]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Fitness.java
@@ -0,0 +1,21 @@
+package ru.job4j.loop;
+
+public class Fitness {
+    public int calc(int ivan, int nik) {
+        int month = 0;
+        while (ivan < nik) {
+                ivan *= 3;
+                nik *=2;
+                month++;
+                if (ivan >= nik) {
+                    break;
+                }
+            }
+        return month;
+        }
+
+    }
+
+
+
+
