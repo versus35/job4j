6c93fe1ac26fa88bf3548f8967c881a4493105b9
BulankinViktor <bulankin.vs@gmail.com>
2019-09-09 16:42:23 +0300

    Fit [159474]

--- chapter_001/src/main/java/ru/job4j/calculator/package-info.java
+++ chapter_001/src/main/java/ru/job4j/calculator/package-info.java
@@ -1,9 +1,22 @@
 /**
- * Packege for calculator
+ * Package for calculator
  * @author Viktor Bulankin
  * version 1.0
  */
-
-
-
 package ru.job4j.calculator;
+    /**
+     * Method manWeight.
+     * @param height
+     * @return (идеальный вес мужчины по формуле).
+    }
+
+    /**
+     *
+     * @param height
+     * @return (идальный вес женщины по фомуле).
+     */
+
+    /**
+     * Main.
+     * @param args
+     */
