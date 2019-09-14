2f2e8b50f5c5dcbf71f359785dda675d47205cf9
BulankinViktor <bulankin.vs@gmail.com>
2019-09-14 18:18:57 +0300

    Matrix [#159488]

---
+++ chapter_001/src/main/java/ru/job4j/array/Matrix.java
@@ -0,0 +1,13 @@
+package ru.job4j.array;
+
+public class Matrix {
+    public int[][] multiple(int size) {
+        int[][] table = new int[size][size];
+        for (int i = 0; i < size ; i++){
+           for (int j = 0; j < size; j++){
+               table [i] [j]  = (i +1) * (j + 1) ;
+            }
+        }
+        return table;
+    }
+}
