3fdb167909894b6694f95ad955ff4b0c2d56aa38
BulankinViktor <bulankin.vs@gmail.com>
2019-09-24 20:28:37 +0300

    SqMax [#159502] : Исправление ошибки

--- chapter_001/src/main/java/ru/job4j/condition/SqMax.java
+++ chapter_001/src/main/java/ru/job4j/condition/SqMax.java
@@ -4,17 +4,11 @@ package ru.job4j.condition;
 public class SqMax {
 	public static int max(int first, int second, int third, int forth) {
 		int result = forth;
-		if (first > second) {
-			if (first > third) {
-				if (first > forth) {
-					result = third;
-				}
-			}
-		} else if (second > third) {
-			if (second > forth) {
-				result = third;
-			}
-		} else if (third < forth) {
+		if (first > second && first > third && first > forth) {
+			result = first;
+		} else if (second > third && second > forth) {
+			result = second;
+		} else if (third > forth) {
 			result = third;
 		}
 		return result;
--- chapter_001/src/test/java/ru/job4j/condition/SqMaxTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/SqMaxTest.java
@@ -16,19 +16,31 @@ public class SqMaxTest {
 	@Test
 	public void secondTo3To4To1() {
 		int result = SqMax.max(2, 3, 4, 1);
-		assertThat(result, is(1));
+		assertThat(result, is(4));
 	}
 
 	@Test
 	public void firstTo3To4To2() {
-		int result = SqMax.max(1, 3, 4, 2);
-		assertThat(result, is(2));
+		int result = SqMax.max(1, 5, 4, 2);
+		assertThat(result, is(5));
 	}
 
 	@Test
 	public void forthTo3To7To6() {
 		int result = SqMax.max(4, 3, 7, 6);
-		assertThat(result, is(6));
+		assertThat(result, is(7));
+	}
+
+	@Test
+	public void tenTo3To7To6() {
+		int result = SqMax.max(10, 3, 7, 6);
+		assertThat(result, is(10));
+	}
+
+	@Test
+	public void twentyToTo7To6() {
+		int result = SqMax.max(4, 20, 7, 6);
+		assertThat(result, is(20));
 	}
 
 }
