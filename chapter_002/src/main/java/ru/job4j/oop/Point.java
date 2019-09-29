d75e5bbadf010d9d757a791c8abd3fea8a0cc0b9
BulankinViktor <bulankin.vs@gmail.com>
2019-09-29 18:40:26 +0300

    Point [#175085] Расстояние между точками в трехмерном пространстве

--- chapter_002/src/main/java/ru/job4j/oop/Point.java
+++ chapter_002/src/main/java/ru/job4j/oop/Point.java
@@ -3,26 +3,49 @@ import static java.lang.Math.sqrt;
 import static java.lang.Math.pow;
 
 public class Point {
-	private int x1;
-	private int y1;
-	private int x2;
-	private int y2;
-	public Point(int first, int second, int third, int forth){
-		this.x1 = first;
-		this.y1 = second;
-		this.x2 = third;
-		this.y2 = forth;
+	private int x;
+	private int y;
+	private int z;
+	public  Point(int first, int second){
+		this.x = first;
+		this.y = second;
+	}
+	public  Point(int first, int second, int third){
+		this.x = first;
+		this.y = second;
+		this.z = third;
+
+	}
+	public void info() {
+		System.out.println(String.format("Point[%s, %s, %s]", this.x, this.y, this.z));
 	}
 
 	public  double distance(Point that){
-		return Math.sqrt(pow(this.x1 - that.x2, 2) + pow (this.y1 - that.y2, 2));
+		return Math.sqrt(Math.pow(this.x - that.x, 2) + Math.pow(this.y - that.y, 2));
+	}
+
+	public double distance3D(Point that){
+		return  Math.sqrt(Math.pow(that.x - this.x, 2) + Math.pow(that.y - this.y,2) + Math.pow(that.z - this.z, 2));
+
 	}
 
 	public static void main(String[] args) {
-		Point a = new Point(2, 3, 5, 1);
-		Point b = new Point(4, 2, 1, 5);
+		Point a = new Point(2, 3, 0);
+		Point b = new Point(0, 2, 4);
+		Point c = new Point(3, 0, 2);
 		double dist = a.distance(b);
+		a.info();
 		System.out.println(dist);
+		double dist1 = a.distance(c);
+		b.info();
+		System.out.println(dist1);
+		double dist2 = a.distance3D(c);
+		c.info();
+		System.out.println(dist2);
+
+
+
 
 	}
+
 }
