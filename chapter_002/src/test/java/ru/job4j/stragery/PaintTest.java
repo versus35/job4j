e6c8d922729293b5e47592579869c708cfc1c1e7
BulankinViktor <bulankin.vs@gmail.com>
2019-10-06 22:49:49 +0300

    PaintTest [#175066] Рефакторинг теста @Before @After

--- chapter_002/src/test/java/ru/job4j/stragery/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/stragery/PaintTest.java
@@ -1,7 +1,8 @@
 package ru.job4j.stragery;
 
 import org.junit.Test;
-
+import org.junit.After;
+import org.junit.Before;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
@@ -10,20 +11,30 @@ import static org.junit.Assert.assertThat;
 
 public class PaintTest {
 	private String std = System.lineSeparator();
+	// поле содержит дефолтный вывод в консоль.
+	private final PrintStream stdout = System.out;
+	// буфер для результата.
+	private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+	@Before
+	public void loadOutput() {
+		System.out.println("execute before method");
+		System.setOut(new PrintStream(this.out));
+	}
+
+	@After
+	public void backOutput() {
+		System.setOut(this.stdout);
+		System.out.println("execute after method");
+	}
+
 
 	@Test
 	public void whenDrawSquare() {
-		// получаем ссылку на стандартный вывод в консоль.
-		PrintStream stdout = System.out;
-		// Создаем буфур для хранения вывода.
-		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		//Заменяем стандартный вывод на вывод в пямять для тестирования.
-		System.setOut(new PrintStream(out));
-		// выполняем действия пишушиее в консоль.
 		new Paint().draw(new Square());
 		// проверяем результат вычисления
 		assertThat(
-				new String(out.toByteArray()),
+				this.out.toString(),
 				is(
 						new StringBuilder()
 								.append("□□□□□□□□□").append(std)
@@ -40,17 +51,10 @@ public class PaintTest {
 
 	@Test
 	public void whenTriangle() {
-		// получаем ссылку на стандартный вывод в консоль.
-		PrintStream stdout = System.out;
-		// Создаем буфур для хранения вывода.
-		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		//Заменяем стандартный вывод на вывод в пямять для тестирования.
-		System.setOut(new PrintStream(out));
-		// выполняем действия пишушиее в консоль.
 		new Paint().draw(new Triangle());
 		// проверяем результат вычисления
 		assertThat(
-				new String(out.toByteArray()),
+				this.out.toString(),
 				is(
 						new StringBuilder()
 								.append("    △").append(std)
