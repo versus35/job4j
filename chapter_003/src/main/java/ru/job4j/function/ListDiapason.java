a2a33327d8f7061d2d3785e6da3beb0de487f4ef
BulankinViktor <bulankin.vs@gmail.com>
2019-11-07 19:47:01 +0300

    ListDiaposon [#174915] Подсчет функции в диапозоне

--- chapter_003/src/main/java/ru/job4j/function/ListDiapason.java
+++ chapter_003/src/main/java/ru/job4j/function/ListDiapason.java
@@ -5,12 +5,7 @@ import java.util.List;
 import java.util.function.Function;
 
 public class ListDiapason {
-	public interface Operation {
-		double calc(int start, int end);
-
-	}
-
-	List<Double> diapason(int start, int end, Function<Double, Double> func) {
+	public List<Double> diapason(int start, int end, Function<Double, Double> func) {
 		List<Double> result = new ArrayList<>();
 		for (int value = start; value < end; value++) {
 			result.add(func.apply((double) value));
