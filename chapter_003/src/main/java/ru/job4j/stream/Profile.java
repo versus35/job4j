72934bbe0c33db823b548681115dd447b394e719
BulankinViktor <bulankin.vs@gmail.com>
2019-11-11 21:13:18 +0300

    Profile [#174925] Уникальность элементов и сортировка

--- chapter_003/src/main/java/ru/job4j/stream/Profile.java
+++ chapter_003/src/main/java/ru/job4j/stream/Profile.java
@@ -1,5 +1,6 @@
 package ru.job4j.stream;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -12,6 +13,13 @@ public class Profile {
 
 	public static List<Address> collect(List<Profile> profiles) {
 		return profiles.stream().map(x -> x.address).collect(Collectors.toList());
+	}
+	public static List<Address> whenCollect(List<Profile> profiles) {
+		return profiles.stream()
+				.map(x -> x.address)
+				.sorted(Comparator.comparing(Address::getCity))
+				.distinct()
+				.collect(Collectors.toList());
 	}
 
 	public Address getAddress() {
--- chapter_003/src/test/java/ru/job4j/stream/ProfileTest.java
+++ chapter_003/src/test/java/ru/job4j/stream/ProfileTest.java
@@ -20,4 +20,17 @@ public class ProfileTest {
 		List<Address> result = Profile.collect(profiles);
 		assertThat(result, is(expect));
 	}
+
+	@Test
+	public void whenUniqueSort() {
+		Address address = new Address("Moscow", "Lenina", 36, 12);
+		Address address1 = new Address("S.P", "Nevskii", 12, 13);
+		Profile profile1 =  new Profile(address);
+		Profile profile2 = new Profile(address1);
+		Profile profile3 = new Profile(address1);
+		List<Profile> profiles = new ArrayList<>(Arrays.asList(profile1, profile2, profile3));
+		List<Address> expected = new ArrayList<>(Arrays.asList(address, address1));
+		List<Address> result = Profile.whenCollect(profiles);
+		assertThat(result, is(expected));
+	}
 }
