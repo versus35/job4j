c177970e0378c42decc7b6893d9d18669607f18a
BulankinViktor <bulankin.vs@gmail.com>
2019-12-08 21:25:28 +0300

    EvenIterator [208616] Создать итератор четные числа

--- chapter_004/src/main/java/iterator/EvenNumbersIterator.java
+++ chapter_004/src/main/java/iterator/EvenNumbersIterator.java
@@ -6,6 +6,7 @@ import java.util.NoSuchElementException;
 
 /**
  * Итератор для четных чисел.
+ *
  * @author Bulankin Viktor.
  * @version $Id$.
  * @since 05.12.2019.
@@ -23,6 +24,7 @@ public class EvenNumbersIterator implements Iterator {
 
     /**
      * Конструктор класса.
+     *
      * @param array Обрабатываемый массив.
      */
 
@@ -34,13 +36,14 @@ public class EvenNumbersIterator implements Iterator {
 
     /**
      * Метод проверяет наличие следующего четного числа.
+     *
      * @return Результат проверки.
      */
 
     @Override
     public boolean hasNext() {
         boolean hasNext = false;
-        int currentIndex = this.index;
+//        int currentIndex = this.index;
 //        while (currentIndex < array.length) {
 //            if (this.array[currentIndex] % 2 == 0) {
 //                hasNext = true;
@@ -51,33 +54,27 @@ public class EvenNumbersIterator implements Iterator {
         if (index < array.length) {
             for (int i = index; i < array.length; i++) {
                 if (array[i] % 2 == 0) {
+                    index = i;
                     hasNext = true;
                     break;
                 }
             }
         }
-
         return hasNext;
     }
 
     /**
      * Метод возвращает следующий элемент массива, содержащий четное число.
+     *
      * @return Элемент массива.
      */
 
     @Override
-    public Object next() {
-        int numbers = 0;
-        while (true) {
-            if (!hasNext()) {
-                throw new NoSuchElementException();
-            }
-            numbers = this.array[this.index];
-            this.index++;
-            if (numbers % 2 == 0) {
-                break;
-            }
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
         }
-        return numbers;
+        return array[index++];
     }
 }
+
