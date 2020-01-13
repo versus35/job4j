46cde2596f0f5540fe6d75406ea62841512b2b66
BulankinViktor <bulankin.vs@gmail.com>
2020-01-13 23:57:05 +0300

    SimpleQueue [#208608] Очередь на двух стеках

--- chapter_004/src/main/java/ru/job4j/list/SimpleQueue.java
+++ chapter_004/src/main/java/ru/job4j/list/SimpleQueue.java
@@ -2,13 +2,31 @@ package ru.job4j.list;
 
 public class SimpleQueue<T>{
 
-    private SimpleListStack<T> list;
+    private SimpleListStack<T> first;
+    private SimpleListStack<T> second;
 
     public SimpleQueue() {
-        this.list = new SimpleListStack<>();
+        this.first = new SimpleListStack<>();
+        this.second = new SimpleListStack<>();
+    }
+
+    public boolean isEmpty() {
+        if (second.isEmpty && first.isEmpty) {
+            return true;
+        }
+        return false;
+    }
+
+    public void push(T data) {
+        this.first.push(data);
     }
 
     public T poll() {
-        return list.d
+        if (second.isEmpty()) {
+            while (!first.isEmpty()) {
+                second.push(first.poll());
+            }
+        }
+        return second.poll();
     }
 }
